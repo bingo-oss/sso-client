20492500718a8d89c80e27f0cb6752f9e0aed24c
Kael <penghj@bingosoft.net>
2017-04-19 14:58:03 +0800

    remove class comment

--- src/main/java/net/bingosoft/oss/ssoclient/internal/JSON.java
+++ src/main/java/net/bingosoft/oss/ssoclient/internal/JSON.java
@@ -19,9 +19,6 @@ import com.alibaba.fastjson.JSONObject;
 
 import java.util.Map;
 
-/**
- * Created by kael on 2017/4/17.
- */
 public class JSON {
     public static Map<String, Object> decodeToMap(String json){
         JSONObject obj = com.alibaba.fastjson.JSON.parseObject(json);
--- src/main/java/net/bingosoft/oss/ssoclient/internal/Strings.java
+++ src/main/java/net/bingosoft/oss/ssoclient/internal/Strings.java
@@ -15,9 +15,6 @@
  */
 package net.bingosoft.oss.ssoclient.internal;
 
-/**
- * Created by kael on 2017/4/18.
- */
 public class Strings {
     public static String nullOrToString(Object o){
         if(o == null){
